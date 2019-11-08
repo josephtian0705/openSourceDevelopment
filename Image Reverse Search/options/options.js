6d9c3a83d7e6a9912fca2bf5479cc402389a45be
josephtian0705 <40748336+josephtian0705@users.noreply.github.com>
2019-11-08 18:11:42 +0800

    Show image beside image URL
    Image to show beside URL link to let the user search the image.

--- Image Reverse Search/options/options.js
+++ Image Reverse Search/options/options.js
@@ -384,7 +384,21 @@ function handleFileSelect(evt) {
       if (!f.type.match('image.*')) {
         continue;
       }
-	    *******************************************************************
+	var reader = new FileReader();
+	
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var span = document.createElement('span');
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+          document.getElementById('list').insertBefore(span, null);
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+	  reader.readAsDataURL(f);
     }
   }
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
