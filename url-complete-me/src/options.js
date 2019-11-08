346e0130fb617e61946d7157716c759f906a4afc
josephtian0705 <40748336+josephtian0705@users.noreply.github.com>
2019-11-08 18:05:02 +0800

    Save bookmark function added
    Able to save the bookmark by the website name and URL

--- url-complete-me/src/options.js
+++ url-complete-me/src/options.js
@@ -1,30 +1,41 @@
+document.getElementById('bookmarkForm').addEventListener('submit', saveBookmark);
 
-/*
-  Save the configured URL to sync storage, which syncs across browsers if the user is logged in
-*/
-function saveOptions(event) {
-  var userInput = document.querySelector("#user-url").value;
-  browser.storage.sync.set({
-    userUrl: userInput
-  });
-  event.preventDefault();
-  restoreOptions(); // show the user what they just saved
+
+function saveBookmark(e) {
+
+    var bookmarkName = document.getElementById('bookmarkName').value;
+    var bookmarkURL = document.getElementById('bookmarkURL').value;
+
+    if (!validateForm(bookmarkName, bookmarkURL)) {
+
+        return false;
+    }
+
+    var bookmark = {
+        name: bookmarkName,
+        url: bookmarkURL
+    }
+
+    if (localStorage.getItem('bookmarks') === null) {
+
+        var bookmarks = [];
+
+        bookmarks.push(bookmark);
+
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    } else {
+
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+        bookmarks.push(bookmark);
+
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+
+    fetchBookmarks();
+
+    e.preventDefault();
+
 }
-
-/*
-  Load from storage to show the user what they"ve configured.
-  Populates an example below the input box.
-*/
-function restoreOptions() {
-  var storageItemPromise = browser.storage.sync.get("userUrl");
-  storageItemPromise.then((storageItem) => {
-    console.log("storageItem =", storageItem);
-
-    document.querySelector("#display-url").innerText = storageItem.userUrl.replace("{}", "my-clipboard-contents-here");
-    document.querySelector("#user-url").value = storageItem.userUrl || "Nothing set";
-  });
-}
-
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
 
